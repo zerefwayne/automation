46fc275c6d30c69d09ece06865b76b182a49df39
Aayush Joglekar <aayushjog@gmail.com>
2020-09-25 14:33:28 +0530

    converted program to runnable script

--- 2-sort-album-by-people/script.py
+++ 2-sort-album-by-people/script.py
@@ -8,11 +8,11 @@ import numpy as np
 import shutil
 
 known_people = []
-original_image_path = os.path.join(os.getcwd(), 'photos/friends.jpg')
+allowed_extensions = ['.jpg', '.jpeg', '.svg', '.png']
+default_output_folder = 'album'
 
 
 def read_image(image_name, image_path):
-
     image = face_recognition.load_image_file(image_path)
     name = image_name.split('.')[0]
 
@@ -20,20 +20,17 @@ def read_image(image_name, image_path):
 
 
 def encode_people(people):
-
     print("Learning faces of {} people".format(len(people)))
 
     for i in range(len(people)):
-
         person = people[i]
-        print("Processing face of {} ({}/{})".format(person['name'], i+1, len(people)))
+        print("Processing face of {} ({}/{})".format(person['name'], i + 1, len(people)))
         person["encoding"] = face_recognition.face_encodings(person['image'])[0]
 
     print("Successfully learnt all faces.")
 
 
 def recognize_in_image(people, image_path):
-
     image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
 
     face_locations = face_recognition.face_locations(image)
@@ -70,12 +67,7 @@ def recognize_in_image(people, image_path):
 
 
 def add_image_to_directories(image_path, names):
-
-    curr_dir = os.getcwd()
-    sorted_album_dir = os.path.join(curr_dir, "testing/album")
-
-    if not os.path.exists(sorted_album_dir):
-        os.mkdir(sorted_album_dir)
+    sorted_album_dir = default_output_folder
 
     for name in names:
         name_path = os.path.join(sorted_album_dir, name)
@@ -88,33 +80,69 @@ def add_image_to_directories(image_path, names):
 
 if __name__ == "__main__":
 
-    known_dir = os.path.join(os.getcwd(), 'known')
-    photos_dir = os.path.join(os.getcwd(), 'photos')
-
-    if not os.path.exists(known_dir) or not os.path.exists(photos_dir):
-        print("Directory of known people: {} doesn't exist.".format('known'))
+    if len(sys.argv) < 3:
+        print("Arguments not provided.")
+        print(
+            "Tip: Please run the script with `script.py <relative/absolute path of known people> <relative/absolute path of photos to be sorted>`")
         sys.exit(1)
 
-    for person in os.listdir(known_dir):
+    known_path_input = sys.argv[1]
+    photos_path_input = sys.argv[2]
 
-        path = os.path.join(known_dir, person)
-        person_detail = read_image(person, path)
-        known_people.append(person_detail)
+    if len(known_path_input) == 0:
+        print("Invalid known path.")
+        sys.exit(1)
+
+    if len(photos_path_input) == 0:
+        print("Invalid photos path.")
+        sys.exit(1)
+
+    known_path_input = os.path.abspath(known_path_input)
+
+    if not os.path.exists(known_path_input) or not os.path.isdir(known_path_input):
+        print("Cannot recognize the known path provided.")
+        sys.exit(1)
+
+    photos_path_input = os.path.abspath(photos_path_input)
+
+    if not os.path.exists(photos_path_input) or not os.path.isdir(photos_path_input):
+        print("Cannot recognize the photos path provided.")
+        sys.exit(1)
+
+    for person in os.listdir(known_path_input):
+        path = os.path.join(known_path_input, person)
+
+        if os.path.isfile(path) and os.path.splitext(path)[1] in allowed_extensions:
+            person_detail = read_image(person, path)
+            known_people.append(person_detail)
+        else:
+            print("Error processing person: {}".format(person))
 
     encode_people(known_people)
 
     print("Sorting images")
 
-    found_photos = os.listdir(photos_dir)
+    if not os.path.exists(os.path.abspath(default_output_folder)):
+        print("Creating folder {} for output.".format(default_output_folder))
+        os.mkdir(os.path.abspath(default_output_folder))
+
+    found_photos = os.listdir(photos_path_input)
 
     for i, photo in zip(range(len(found_photos)), found_photos):
 
-        print("Processing ({}/{}): {}".format(i+1, len(found_photos), photo))
+        print("Processing ({}/{}): {}".format(i + 1, len(found_photos), photo))
 
-        original_image_path = os.path.join(photos_dir, photo)
+        original_image_path = os.path.join(photos_path_input, photo)
 
-        recognized_names = recognize_in_image(known_people, original_image_path)
+        if os.path.isfile(original_image_path) and os.path.splitext(original_image_path)[1] in allowed_extensions:
 
-        add_image_to_directories(original_image_path, recognized_names)
+            recognized_names = recognize_in_image(known_people, original_image_path)
+
+            if len(recognized_names) == 0:
+                print("Warning: No persons recognized.")
+
+            add_image_to_directories(original_image_path, recognized_names)
+        else:
+            print("Error processing photo: {}".format(photo))
 
     print("Successfully sorted all photos.")
